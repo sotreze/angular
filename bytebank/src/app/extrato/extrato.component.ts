a6622baa6dba2d0dc06a7a8b85b3196148f40497
Marcio <sotreze@yahoo.com.br>
2022-04-06 15:33:19 -0300

    Exibindo lista com ngFor

--- bytebank/src/app/extrato/extrato.component.ts
+++ bytebank/src/app/extrato/extrato.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-extrato',
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExtratoComponent implements OnInit {
 
+  @Input() transferencias: any[];
+
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit() {}
 
 }
