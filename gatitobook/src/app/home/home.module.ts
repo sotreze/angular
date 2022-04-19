b1041a7e2dc98374576659b7083a6f36052d400a
Marcio <sotreze@yahoo.com.br>
2022-04-19 15:21:07 -0300

    Validação de formulário

--- gatitobook/src/app/home/home.module.ts
+++ gatitobook/src/app/home/home.module.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { MensagemModule } from '../componentes/mensagem/mensagem.module';
 
 
 @NgModule({
@@ -11,10 +13,7 @@ import { LoginComponent } from './login/login.component';
     HomeComponent,
     LoginComponent
   ],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ],
+  imports: [CommonModule, HomeRoutingModule, FormsModule, MensagemModule],
   exports: [HomeComponent]
 })
 export class HomeModule { }
