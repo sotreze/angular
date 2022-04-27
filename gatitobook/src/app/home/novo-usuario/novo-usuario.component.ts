aa468731f662fc9bfb1d58df78a4bd582ef0cd67
Marcio <sotreze@yahoo.com.br>
2022-04-27 14:37:51 -0300

    Cadastro de usuário

--- gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { minusculoValidator } from './minusculo.validator';
 import { NovoUsuario } from './novo-usuario';
 import { NovoUsuarioService } from './novo-usuario.service';
@@ -18,7 +19,8 @@ export class NovoUsuarioComponent implements OnInit {
   constructor(
       private formBuilder: FormBuilder, 
       private novoUsuarioService: NovoUsuarioService,
-      private usuarioExistenteService: UsuarioExisteService
+      private usuarioExistenteService: UsuarioExisteService,
+      private router: Router
     ) { }
 
   ngOnInit(): void {
@@ -36,8 +38,15 @@ export class NovoUsuarioComponent implements OnInit {
   }
 
   cadastrar() {
-    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario; 
-    console.log(novoUsuario);
+    if (this.novoUsuarioForm.valid) {
+      const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario; 
+      this.novoUsuarioService.cadastraNovoUsuaro(novoUsuario).subscribe(() => {
+        this.router.navigate(['']);
+      },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }  
   }
-
 }
