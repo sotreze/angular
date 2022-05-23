3f39f5c3cd3cc7ebd541c5abe63cfc13cf21c587
Marcio <sotreze@yahoo.com.br>
2022-05-23 15:09:38 -0300

    NG_VALUE_ACCESSOR

--- A11Y-P1/src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
+++ A11Y-P1/src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -1,29 +1,55 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, forwardRef, Input, OnInit, Output } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-yes-no-button-group',
   templateUrl: './yes-no-button-group.component.html',
-  styleUrls: ['./yes-no-button-group.component.scss']
+  styleUrls: ['./yes-no-button-group.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: forwardRef(() => YesNoButtonGroupComponent)
+    }
+  ]
 })
-export class YesNoButtonGroupComponent implements OnInit {
-
+export class YesNoButtonGroupComponent implements OnInit, ControlValueAccessor {
   @Input() public value: string = null;
   @Input() public label = '';
   @Output() public valueChange = new EventEmitter<string>();
   public options = YesNoButtonGroupOptions;
+  public onChange = (value: string) => {};
+  public OnTouched = () => {};
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public writeValue(value: string): void {
+    this.value = value;
+    this.onChange(this.value);
+    this.valueChange.emit(this.value);
+  }
+  public registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
   }
 
-  public activate(value: string): void{
-    this.value = value;
-    this.valueChange.emit(this.value);
+  public registerOnTouched(fn: () => void): void {
+    this.OnTouched = fn;
+  }
+
+  public setDisabledState?(isDisabled: boolean): void {
+    throw new Error('Method not implemented.');
+  }
+
+
+
+  public activate(value: string): void {
+    this.writeValue(value);
   }
 }
 
 enum YesNoButtonGroupOptions {
   YES = 'yes',
-  NO = 'no'
+  NO = 'no',
 }
