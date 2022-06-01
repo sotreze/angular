287d5a4c5795bfe9a12aac75df67dd79e6086f64
Marcio <sotreze@yahoo.com.br>
2022-06-01 15:21:23 -0300

    Injetando templates

--- A11Y-P2/src/app/app.component.ts
+++ A11Y-P2/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { ModalRef, ModalService } from './shared/components/modal/services/modal.services';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild('modal') public modalTemplatRef: TemplateRef<any>;
   title = 'a11y-p2';
+  public firstName= 'Marcio';
+  public modalRef: ModalRef;
+
+  constructor(private modaService: ModalService) {}
+
+  public show(): void {
+    this.modalRef = this.modaService.open({
+      templateRef: this.modalTemplatRef,
+      title: 'User Details'
+    });
+  }
 }
--- A11Y-P2/src/app/shared/components/modal/services/modal.services.ts
+++ A11Y-P2/src/app/shared/components/modal/services/modal.services.ts
@@ -1,4 +1,21 @@
-import { Injectable } from "@angular/core";
+import { Injectable, TemplateRef } from "@angular/core";
 
 @Injectable()
-export class ModalService {}
+export class ModalService {
+
+    public open(config: ModalConfig): ModalRef {
+        console.log('open called');
+        return new ModalRef();
+    }
+}
+
+export interface ModalConfig {
+    templateRef: TemplateRef<any>;
+    title: string;
+}
+
+export class ModalRef {
+    public close(): void {
+        console.log('close called');
+    }
+}
