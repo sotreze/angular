028f87a7de0cadebf0ad05490c756553508b7ac5
Marcio <sotreze@yahoo.com.br>
2022-06-03 15:27:28 -0300

    AttachView

--- A11Y-P2/src/app/shared/components/modal/services/modal.services.ts
+++ A11Y-P2/src/app/shared/components/modal/services/modal.services.ts
@@ -1,6 +1,7 @@
 import { ModalComponent } from './../modal.component';
-import { ComponentFactory, ComponentFactoryResolver, Injectable, Injector } from '@angular/core';
+import { ComponentFactory, ComponentFactoryResolver, ComponentRef, Injectable, Injector } from '@angular/core';
 import { ModalConfig } from '../interfaces/modal-config';
+import { BodyInjectorService } from 'src/app/shared/services/body-injector';
 
 @Injectable()
 export class ModalService {
@@ -9,20 +10,32 @@ export class ModalService {
 
   constructor(
     componentFactoryResolver: ComponentFactoryResolver,
-    private injector: Injector
+    private injector: Injector,
+    private bodyInjector: BodyInjectorService
     ) {
     this.componentFactory = componentFactoryResolver.resolveComponentFactory(ModalComponent);
   }
 
   public open(config: ModalConfig): ModalRef {
-    const componentRef = this.componentFactory.create(this.injector);
+    const componentRef = this.createComponentRef();
+    componentRef.instance.config = config;
+    console.log(componentRef.instance);
     console.log('open called');
-    return new ModalRef();
+    this.bodyInjector.stackBeforeAppRoot(componentRef);
+    return new ModalRef(componentRef);
+  }
+
+  private createComponentRef(): ComponentRef<ModalComponent> {
+    return this.componentFactory.create(this.injector);
   }
 }
 
 export class ModalRef {
+
+  constructor(private componentRef: ComponentRef<ModalComponent>) {}
+
   public close(): void {
     console.log('close called');
+    this.componentRef.destroy();
   }
 }
