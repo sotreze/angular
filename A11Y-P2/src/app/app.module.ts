362ab4d3378023fdea82e779357f57bc89f97a15
Marcio <sotreze@yahoo.com.br>
2022-08-17 10:15:36 -0300

    Lapidando a descricão dos testes

--- A11Y-P2/src/app/app.module.ts
+++ A11Y-P2/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ModalModule } from './shared/components/modal/modal.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     ModalModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- A11Y-P2/src/app/shared/components/modal/modal.module.ts
+++ A11Y-P2/src/app/shared/components/modal/modal.module.ts
@@ -2,10 +2,16 @@ import { ModalService } from './services/modal.services';
 import { ModalComponent } from './modal.component';
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { FocusTrapModule } from '../../directives/focus-trap/focus-trap.module';
+import { FocusBackModule } from '../../directives/focus-back/focus-back.module';
 
 @NgModule({
   declarations: [ModalComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FocusTrapModule,
+    FocusBackModule
+  ],
   exports: [ModalComponent],
   providers: [ModalService]
 })
