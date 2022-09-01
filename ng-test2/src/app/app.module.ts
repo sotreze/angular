5312c49e728f0c97724378df5982b769c3844305
Marcio <sotreze@yahoo.com.br>
2022-09-01 14:04:02 -0300

    Conclusão

--- ng-test2/src/app/app.module.ts
+++ ng-test2/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LikeWidgetModule } from './shared/components/like-widget/like-widget.module';
-import { PhotoFrameModule } from './shared/components/photo-frame/photo-frame.module';
+import { PhotoListModule } from './components/photo-list/photo-list.module';
 
 @NgModule({
   declarations: [
@@ -13,8 +13,8 @@ import { PhotoFrameModule } from './shared/components/photo-frame/photo-frame.mo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LikeWidgetModule,
-    PhotoFrameModule
+    HttpClientModule,
+    PhotoListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
